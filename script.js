7638230dc5c67bf501a5e8d37f91b246f25244c3
JamesDCowley <jcowley04@hotmail.com>
2024-07-11 11:26:43 -0500

    added time changing

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const dayjs = require('dayjs');
-dayjs.format();
+
+var updateTime = () => {
+    const time = document.getElementById('time');
+    const date = document.getElementById('date');
+    const timezone = document.getElementById('timezone');
+
+    time.innerText = dayjs.format('hh:mm:ss');
+    setTimeout(updateTime(), 1000);
+}
