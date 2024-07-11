5f35762a8995786116b49d0e268a3d84c8a64c95
JamesDCowley <wuz7@txstate.edu>
2024-07-11 18:05:05 -0500

    timezone works

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const dayjs = require('dayjs');
+var dayjs = require('dayjs');
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone');
 
 /* Get and update time every second */
 var interval = setInterval(() => {
     const time = document.getElementById('time');
     const date = document.getElementById('date');
-    const timezone = document.getElementById('timezone');
+    const timezoneElem = document.getElementById('timezone');
 
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+    
     time.innerText = dayjs().format('HH:mm:ss');
     date.innerText = dayjs().format('dddd, D MMMM, YYYY');
+    timezoneElem.innerText = dayjs.tz.guess();
 }, 1000);
